06c92bf9de5276c2abb15d69d33f25282bb762e1
Sergey <falkr4m@yandex.ru>
2021-11-04 08:52:14 +0300

    Some changes in story writing

---
+++ components/button/Button.js
@@ -0,0 +1,48 @@
+import './Button.scss'
+import button from './Button.hbs'
+import buttonIcon from './ButtonIcon.hbs'
+
+export class Button {
+  constructor({
+                label,
+                color = "black",
+                rounded = false,
+                size = "md",
+                icon = '',
+              }) {
+    this.size = size;
+    this.rounded = rounded;
+    this.color = color;
+    this.label = label;
+    if (icon !== '') {
+      this.icon = icon;
+    }
+  }
+
+  render() {
+    return button({
+      color: this.color,
+      size: this.size,
+      rounded: this.rounded,
+      label: this.label,
+      icon: this.icon,
+    });
+  };
+}
+
+export class ButtonIcon {
+  constructor({
+                iconButtonColor = "gray",
+                icon = '',
+              }) {
+    this.color = iconButtonColor;
+    this.icon = icon;
+  }
+
+  render() {
+    return buttonIcon({
+      color: this.color,
+      icon: this.icon,
+    })
+  }
+}
