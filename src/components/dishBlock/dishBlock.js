fe3a66cee5ad629b9ceb26fab0fd817135ebe46e
Sergey <falkr4m@yandex.ru>
2021-11-15 14:43:48 +0300

    Added add Dish profile template for a dish

--- src/components/dishBlock/dishBlock.js
+++ src/components/dishBlock/dishBlock.js
@@ -1,28 +1,20 @@
 import styles from './dishBlock.scss'
 import dishBlockTemplate from './dishBlock.hbs'
 
-export class DishBlock {
-  constructor({
-                id = "",
-                img = "",
-                name = "",
-                cost = "",
-                ccal = "",
-              }) {
-    this.id = id;
-    this.img = img;
-    this.name = name;
-    this.cost = cost;
-    this.ccal = ccal;
-  }
-
-  render() {
-    return dishBlockTemplate({
-      id: this.id,
-      img: this.img,
-      name: this.name,
-      cost: this.cost,
-      ccal: this.ccal,
-    });
-  }
+export function DishBlock({
+                            addDishProfile = false,
+                            id = "",
+                            img = "",
+                            name = "",
+                            cost = "",
+                            ccal = "",
+                          }) {
+  return dishBlockTemplate({
+    addDishProfile: addDishProfile,
+    id: id,
+    img: img,
+    name: name,
+    cost: cost,
+    ccal: ccal,
+  });
 }
--- stories/dishBlock.stories.js
+++ stories/dishBlock.stories.js
@@ -5,13 +5,20 @@ export default {
   component: DishBlock,
 }
 
-export const Default = (args) => {
-  const block = new DishBlock(args);
-  return block.render();
-}
-
+export const Default = (args) => DishBlock(args);
 
 Default.args = {
+  id: '1',
+  img: 'https://pokatim.ru/uploads/posts/2020-08/1598606636_rpwswmq3ullhobrcmp5evrhuifhl6x5k0nnt8dda.jpeg',
+  name: 'KFC',
+  cost: '123',
+  ccal: '320',
+}
+
+export const AddInRestaurant = (args) => DishBlock(args);
+
+AddInRestaurant.args = {
+  addDishProfile: true,
   id: '1',
   img: 'https://pokatim.ru/uploads/posts/2020-08/1598606636_rpwswmq3ullhobrcmp5evrhuifhl6x5k0nnt8dda.jpeg',
   name: 'KFC',
