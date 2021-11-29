8074aaadc6e6b1cc8cb02e75db399776ee2ff268
Sergey <falkr4m@yandex.ru>
2021-11-29 21:34:06 +0300

    Some visual fixes with text area and card, deleted profile buttons navs

--- src/components/profileButtonsNav/profileButtonsNav.js
+++
@@ -1,6 +0,0 @@
-import styles from './profileButtonsNav.scss'
-import profileButtonsNavTemplate from './profileButtonsNav.hbs'
-
-export const ProfileButtonsNav = (props) => {
-    return profileButtonsNavTemplate(props);
-}
